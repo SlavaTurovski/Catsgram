c726f378d16225e6ebf652649dbdd37c01e2369c
SlavaTurovski <turowscky2018@gmail.com>
2025-05-14 13:02:17 +0300

    catsgram-fisrt-push

--- src/main/java/ru/yandex/practicum/catsgram/model/Image.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Image.java
@@ -1,11 +1,17 @@
 package ru.yandex.practicum.catsgram.model;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.File;
+
+@Data
 @EqualsAndHashCode(of = {"id"})
 public class Image {
+
     private Long id;
     private long postId;
     private String originalFileName;
-    private String filePath;
+    private File filePath;
+
 }
